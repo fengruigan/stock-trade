dde9538c1ad543c98fd0b6b5ee0d2de77dedb5fc
fengruigan <fengruigan@users.noreply.github.com>
2020-07-05 17:11:55 -0400

    submit order to alpaca

--- venv/main.py
+++ venv/main.py
@@ -1,4 +1,15 @@
+import requests
+import json
 import numpy as np
 import alpaca_trade_api as tradeapi
 
-print("hello")
+
+key = 'PKSJQXDQJO4C11AB2HG6'
+secret_key = 'dZ9V3fiTxy7fzzlb1DTuEpsIkJuRRRdvsyKILkwO'
+base_url = 'https://paper-api.alpaca.markets'
+api = tradeapi.REST(key, secret_key, base_url, api_version='v2')
+api.submit_order(symbol="AAPL", qty=1, side="buy", type="market", time_in_force="gtc")
+
+
+
+
