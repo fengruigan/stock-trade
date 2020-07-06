50993a7df96969338d6d515fec73caf2e00d5818
fengruigan <fengruigan@users.noreply.github.com>
2020-07-06 17:01:32 -0400

    complete execution module

--- venv/Lib/Modules/execution.py
+++ venv/Lib/Modules/execution.py
@@ -1,5 +1,11 @@
 """
 module that handles order execution
+
+Most order methods does not take into account open orders
+Therefore, consider canceling all open orders before placing new ones
+or wait for orders to be filled
+
+I think I will put order check in strategies section
 """
 from Lib.Modules.user_config import keys
 import alpaca_trade_api as tradeapi
@@ -7,35 +13,170 @@ import alpaca_trade_api as tradeapi
 
 api = tradeapi.REST(keys.key, keys.secret_key, keys.base_url, api_version="v2")
 
+def order(symbol: str, qty: int, side: str, type: str="market", time_in_force: str="gtc"):
+    """
+    This method is a refactoring of the original alpaca submit_order() method
+
+    :param symbol: asset symbol or ID
+    :param qty: number of shares to order
+    :param side: buy or sell
+    :param type: market, limit, stop, or stop_limit
+    :param time_in_force: day, gtc, opg, cls, ioc, fok
+    :return: returns order id
+    """
+    return api.submit_order(symbol, qty, side, type, time_in_force).id
+
+
 def order_value(symbol: str, value: float):
     """
     Create an order to specified asset for specified amount of value
 
-    :param symbol: asset symbol
+    :param symbol: asset symbol or ID
     :param value: desired amount to order
     :return: returns the id of the placed order if order was placed successfully
 
     Example:
         order_value("AAPL", 1000),
-        if "AAPL"'s ask price is $300, 3 shares of "AAPL" will be placed with the id returned
+        if "AAPL"'s ask price is $300, method will attempt to buy 3 shares of "AAPL" with order id returned
+        order_value("AAPL", -1000),
+        method will attempt to sell 3 shares of "AAPL" with order id returned
+        if value is larger then position market value,
+        method will close position (sell all) for "AAPL" with order id returned
     """
-    if (api.get_account().regt_buying_power > value):
-        askprice = api.get_last_quote(symbol).askprice
-        if (askprice != 0):
-            shares = int(value / askprice)
-            return api.submit_order(symbol, shares, side="buy", type="market", time_in_force="gtc").id
+    if (value == 0):
+        print("Order value cannot be 0")
+        return None
+    elif (value > 0):
+        if (int(api.get_account().buying_power) > value):
+            askprice = api.get_last_quote(symbol).askprice
+            if (askprice != 0):
+                shares = int(value / askprice)
+                if (shares <= 0):
+                    print("Order value too low, purchasing 0 shares")
+                    return None
+                return order(symbol, shares, "buy")
+            else:
+                print("Error reading askprice, askprice = 0")
+                return None
         else:
-            print("Error reading ask price, ask price = 0")
+            print("Error buying " + shares + " shares of " + symbol + ", not enough buying power")
+            return None
     else:
-        print("Error placing order with order_value, not enough buying power")
+        value = -value
+        for position in api.list_positions():
+            if (symbol.__eq__(position.symbol)):
+                if (value >= position.market_value):
+                    return api.close_position(symbol).id
+                else:
+                    bidprice = api.get_last_quote(symbol).bidprice
+                    if (bidprice != 0):
+                        shares = int(value / bidprice)
+                        if (shares == 0):
+                            print("Order value too low, selling 0 shares")
+                            return None
+                        return order(symbol, shares, "sell")
+                    else:
+                        print("Error reading bidprice, bidprice = 0 ")
+                        return None
+        print("Attempted to sell " + symbol + " but position not found")
+        return None
 
 
 def order_percent(symbol:str, percent:float):
     """
     Create an order to specified asset for amount based on the percent of the current portfolio
 
-    :param symbol: asset symbol
-    :param percent: desired percent to order
+    :param symbol: asset symbol or ID
+    :param percent: desired percent to order value between 0 and 1
+    :return: returns the id of the placed order if order was placed successfully
+
+    Example:
+        order_percent("AAPL", 0.5),
+        if "AAPL"'s ask price is $300 and current portfolio value is $1000,
+        method will attempt to buy 1 share of "AAPL" with order id returned
+        order_percent("AAPL", -0.5),
+        method will attempt to sell 1 share of "AAPL" with order id returned
+    """
+    if (abs(percent) > 1):
+        print("Percentage larger than 100%, input smaller percentage")
+        return None
+    if (percent == 0):
+        print("Percentage cannot be 0%")
+        return None
+    value = int(api.get_account().portfolio_value) * percent
+    return order_value(symbol, value)
+
+
+def order_target(symbol:str, target_share:int):
+    """
+    Create orders to target the desired number of shares holding in portfolio
+
+    :param symbol: asset symbol or ID
+    :param share: the target_share share, if target_share is greater than holding, a buy order will be submitted, else
+                  a sell order will be submitted
+    :return: returns the id of the placed order if order was placed successfully
+
+    Example:
+        order_target("AAPL", 10),
+        if current position qty of "AAPL" is 15, method will attempt to sell 5 shares of "AAPL"
+        if current position qty of "AAPL" is 5, method will attempt to buy 5 shares of "AAPL"
+        returning the order id in both cases
+    """
+    for position in api.list_positions():
+        if (symbol.__eq__(position.symbol)):
+            shares = target_share - int(position.qty)
+            if shares == 0:
+                return None
+            elif shares > 0:
+                if (api.get_last_quote(symbol).askprice * shares < int(api.get_account().buying_power)):
+                    return order(symbol, shares, "buy")
+                else:
+                    print(
+                        "Error buying " + shares + " shares of " + symbol + " with order_target, not enough buying power")
+                    return None
+            else:
+                return order(symbol, -shares, "sell")
+
+    if (api.get_last_quote(symbol).askprice * target_share < int(api.get_account().buying_power)):
+        return order(symbol, target_share, "buy")
+    else:
+        print("Error buying " + target_share + " shares of " + symbol + ", not enough buying power")
+        return None
+
+
+def order_target_value(symbol: str, target_value: float):
+    """
+    Create an order to specified asset to target specified amount of value
+
+    :param symbol: asset symbol or ID
+    :param value: desired target amount to order
+    :return: returns the id of the placed order if order was placed successfully
+
+    Example:
+        order_target_value("AAPL", 1000),
+        if current position market value of "AAPL" is 500 and "AAPL"'s ask price is $300,
+        method will attempt to buy 1 share of "AAPL";
+        if current position market value of "AAPL" is 1500,
+        method will attempt to sell 1 share of "AAPL";
+        if currently not holding any share of "AAPL", method will attempt to buy 3 shares of "AAPL"
+    """
+    if (target_value <= 0):
+        print("Target_value must be > 0")
+        return None
+    for position in api.list_positions():
+        if (symbol.__eq__(position.symbol)):
+            value = target_value - float(position.market_value)
+            return order_value(symbol, value)
+
+    return order_value(symbol, target_value)
+
+
+def order_target_percent(symbol: str, target_percent: float):
+    """
+    Create an order to specified asset to target amount based on the percent of the current portfolio
+
+    :param symbol: asset symbol or ID
+    :param percent: desired target percent to order value between 0 and 1
     :return: returns the id of the placed order if order was placed successfully
 
     Example:
@@ -43,26 +184,13 @@ def order_percent(symbol:str, percent:float):
         if "AAPL"'s ask price is $300 and current portfolio value is $1000,
         1 share of "AAPL" will be placed with the id returned
     """
-    if (percent > 1):
-        print("Percentage larger than 1, input smaller percentage")
-        return
-    value = api.get_account().portfolio_value * percent
-    if (api.get_account().regt_buying_power > value):
-        askprice = api.get_last_quote(symbol).askprice
-        if (askprice != 0):
-            shares = int(value / askprice)
-            return api.submit_order(symbol, shares, side="buy", type="market", time_in_force="gtc").id
-        else:
-            print("Error reading ask price, ask price = 0")
-    else:
-        print("Error placing order with order_percent, not enough buying power")
+    if (target_percent > 1):
+        print("Percentage larger than 100%, input smaller target_percent")
+    if (target_percent <= 0):
+        print("Percentage must be > 0")
+    for position in api.list_positions():
+        if (symbol.__eq__(position.symbol)):
+            percent = target_percent - (float(position.market_value) / float(api.get_account().portfolio_value))
+            return order_percent(symbol, percent)
 
-
-def order_target():
-    pass
-
-def order_target_value():
-    pass
-
-def order_target_percent():
-    pass
+    return order_percent(symbol, target_percent)
--- venv/main.py
+++ venv/main.py
@@ -5,26 +5,23 @@ Handles api connection and main functions here
 import numpy as np
 import alpaca_trade_api as tradeapi
 from Lib.Modules import indicators
-from Lib.Modules import execution
+from Lib.Modules import execution as exe
 from Lib.Modules.user_config import keys
 
 # use this method to update user API keys
-keys.update_key(keys, 'PKH1K1LTH0541LAZHXMT', 'r/bnsex3KAFlxYgwM0Z0LtS4Vu3IqLsuPqRCEFEt')
+keys.update_key(keys, 'PKGQG65N7CIYQORL31FA', 'WMB/pHAMbNK7f8KV9WFh6UqJ6q9tKsS5b8UYtueQ')
 api = tradeapi.REST(keys.key, keys.secret_key, keys.base_url, api_version='v2')
 
 # strategies will be imported
 # create control flow below
 
 
-
-
-
-
 # api.submit_order(symbol="AAPL", qty=1, side="buy", type="market", time_in_force="gtc")
 
 # print(execution.order_value("MSFT", 1000))
-
 # price = api.get_last_quote("MSFT")
 # print(price)
-print(api.get_account())
 # print(cfg)
+# print(api.get_position("AMZN"))
+
+
