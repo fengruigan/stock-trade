5bae2225adeb1900e6d7e16597f65c745e2d884f
fengruigan <fengruigan@users.noreply.github.com>
2020-07-06 00:57:15 -0400

    add comment to order_value

--- venv/Lib/Modules/execution.py
+++ venv/Lib/Modules/execution.py
@@ -8,16 +8,27 @@ import alpaca_trade_api as tradeapi
 api = tradeapi.REST(keys.key, keys.secret_key, keys.base_url, api_version="v2")
 
 def order_value(symbol: str, value: float):
-    askprice = api.get_last_quote(symbol).askprice
-    if (askprice != 0):
-        shares = int(value / askprice)
-        return api.submit_order(symbol, shares, side="buy", type="market", time_in_force="gtc")
+    """
+    Create an order to specified asset for specified amount of value
+
+    :param symbol: asset symbol
+    :param value: desired amount to order
+    :return: returns the id of the placed order if order was placed successfully
+
+    Example:
+        order_value("AAPL", 1000),
+        if "AAPL"'s ask price is $300, 3 shares of "AAPL" will be placed with the id returned
+    """
+    if (api.get_account().regt_buying_power > value):
+        askprice = api.get_last_quote(symbol).askprice
+        if (askprice != 0):
+            shares = int(value / askprice)
+            return api.submit_order(symbol, shares, side="buy", type="market", time_in_force="gtc").id
+        else:
+            print("Error reading ask price, ask price = 0")
     else:
-        print("Error reading ask price, ask price = 0")
+        print("Error placing order with order_value, not enough buying power")
 
-
-def order_target():
-    pass
 
 def order_percent():
     pass
--- venv/main.py
+++ venv/main.py
@@ -8,9 +8,16 @@ from Lib.Modules import indicators
 from Lib.Modules import execution
 from Lib.Modules.user_config import keys
 
-
+# use this method to update user API keys
 keys.update_key(keys, 'PKH1K1LTH0541LAZHXMT', 'r/bnsex3KAFlxYgwM0Z0LtS4Vu3IqLsuPqRCEFEt')
 api = tradeapi.REST(keys.key, keys.secret_key, keys.base_url, api_version='v2')
+
+# strategies will be imported
+# create control flow below
+
+
+
+
 
 
 # api.submit_order(symbol="AAPL", qty=1, side="buy", type="market", time_in_force="gtc")
